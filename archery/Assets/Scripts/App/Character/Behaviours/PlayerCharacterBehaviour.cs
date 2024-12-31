577393bd51659be553fe08827cf4b2cc723b536e
Eldar Zakaryaev <e.zakaryaev@eidoloncorp.com>
2024-12-31 13:41:25 +0300

    add level manager

---
+++ archery/Assets/Scripts/App/Character/Behaviours/PlayerCharacterBehaviour.cs
@@ -0,0 +1,28 @@
+using Archery.Core;
+using UnityEngine;
+
+namespace Archery.Character
+{
+    public class PlayerCharacterBehaviour : MonoBehaviour
+    {
+        [SerializeField] private PlayerReferences _references;
+
+        private PlayerCharacter _playerCharacter;
+
+        private void Awake()
+        {
+            enabled = false;
+        }
+
+        public void Initialize(Services services)
+        {
+            _playerCharacter = new PlayerCharacterBuilder(_references, services).Build();
+            enabled = true;
+        }
+
+        private void Update()
+        {
+            _playerCharacter.Update();
+        }
+    }
+}
--- archery/Assets/Scripts/App/Character/Behaviours/PlayerReferences.cs
+++ archery/Assets/Scripts/App/Character/Behaviours/PlayerReferences.cs
@@ -1,5 +1,4 @@
 using System;
-using Archery.Core;
 using UnityEngine;
 
 namespace Archery.Character
@@ -8,28 +7,5 @@ namespace Archery.Character
     public class PlayerReferences
     {
         [field: SerializeField] public Transform LookCameraTransform { get; private set; }
-    }
-
-    public class PlayerCharacterBehaviour : MonoBehaviour
-    {
-        [SerializeField] private PlayerReferences _references;
-
-        private PlayerCharacter _playerCharacter;
-
-        private void Awake()
-        {
-            enabled = false;
-        }
-
-        public void Initialize(Services services)
-        {
-            _playerCharacter = new PlayerCharacterBuilder(_references, services).Build();
-            enabled = true;
-        }
-
-        private void Update()
-        {
-            _playerCharacter.Update();
-        }
     }
 }
